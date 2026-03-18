5675a3a8f1da3149ce86c5766122bd7c954cd984
thyezoliveira <thyezoliveira@gmail.com>
2026-03-18 11:42:41 -0300

    chore: reconfiguração do repositorio, preparando para uma nova versão do app

--- gad.py
+++ gad.py
@@ -6,17 +6,25 @@ from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
+from google.oauth2 import service_account
 
-SCOPES = ["https://www.googleapis.com/auth/admin.directory.user"]
+#SCOPES = [
+#    "https://www.googleapis.com/auth/admin.directory.user",
+#    "https://www.googleapis.com/auth/admin.directory.orgunit",
+#]
+SCOPES = [
+    "https://www.googleapis.com/auth/admin.directory.user"
+]
+
 
 class GoogleAdminDirectory:
     def __init__(self, excel_filename):
-        self.creds = self.authenticate()
+        self.creds = self.authenticate_legacy()
         self.service = build("admin", "directory_v1", credentials=self.creds)
         self.file_path = excel_filename
-        self.defaultPassword = "#osiris123"
+        self.defaultPassword = "provisorio2026"
 
-    def authenticate(self):
+    def authenticate_legacy(self):
         creds = None
         if os.path.exists("token.json"):
             creds = Credentials.from_authorized_user_file("token.json", SCOPES)
@@ -24,10 +32,18 @@ class GoogleAdminDirectory:
             if creds and creds.expired and creds.refresh_token:
                 creds.refresh(Request())
             else:
-                flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    "credentials.json", SCOPES
+                )
                 creds = flow.run_local_server(port=0)
             with open("token.json", "w") as token:
                 token.write(creds.to_json())
+        return creds
+
+    def authenticate(self):
+        creds = service_account.Credentials.from_service_account_file(
+            "credentials.json", scopes=SCOPES
+        ).with_subject("thyezoliveiramonteiro@smec.saquarema.rj.gov.br")
         return creds
 
     def list_users(self, max_results=10):
@@ -47,9 +63,7 @@ class GoogleAdminDirectory:
 
     def get_user_by_fullname(self, full_name):
         results = (
-            self.service.users()
-            .list(customer="my_customer", maxResults=10)
-            .execute()
+            self.service.users().list(customer="my_customer", maxResults=10).execute()
         )
         users = results.get("users", [])
         for user in users:
@@ -57,29 +71,31 @@ class GoogleAdminDirectory:
 
     def get_email_by_fullname(self, full_name):
         results = (
-            self.service.users()
-            .list(customer="my_customer", maxResults=100)
-            .execute()
+            self.service.users().list(customer="my_customer", maxResults=100).execute()
         )
         users = results.get("users", [])
-        
+
         # Normaliza o nome de busca (remove acentos e converte para minúsculas)
-        normalized_search_name = unicodedata.normalize('NFD', full_name.lower()).encode('ASCII', 'ignore').decode('ASCII')
-        
+        normalized_search_name = (
+            unicodedata.normalize("NFD", full_name.lower())
+            .encode("ASCII", "ignore")
+            .decode("ASCII")
+        )
+
         for user in users:
-            if 'name' in user and 'fullName' in user['name']:
-                if user['name']['fullName'] == full_name:
+            if "name" in user and "fullName" in user["name"]:
+                if user["name"]["fullName"] == full_name:
                     print(user)
                 # user_full_name = user['name']['fullName']
             #     # Normaliza o nome do usuário para comparação
-                # normalized_user_name = unicodedata.normalize('NFD', user_full_name.lower()).encode('ASCII', 'ignore').decode('ASCII')
-                
+            # normalized_user_name = unicodedata.normalize('NFD', user_full_name.lower()).encode('ASCII', 'ignore').decode('ASCII')
+
             #     # Verifica se o nome corresponde (busca exata ou parcial)
-                # if user_full_name == full_name:
-                #     print(user)
-                #     return {
-                #         "primaryEmail": user.get("primaryEmail", "")
-                #     }
+            # if user_full_name == full_name:
+            #     print(user)
+            #     return {
+            #         "primaryEmail": user.get("primaryEmail", "")
+            #     }
             #             "fullName": user_full_name,
             #             "givenName": user['name'].get("givenName", ""),
             #             "familyName": user['name'].get("familyName", ""),
@@ -91,16 +107,21 @@ class GoogleAdminDirectory:
             #             "lastLoginTime": user.get("lastLoginTime", ""),
             #             "suspended": user.get("suspended", False)
             #         }
-        
+
         # Se não encontrou o usuário, retorna None
         print(f"Usuário com nome '{full_name}' não encontrado.")
         return None
-    
+
     def list_users_by_org_unit(self, org_unit_path, max_results=10):
         print(f"Getting users from organizational unit: {org_unit_path}")
         results = (
             self.service.users()
-            .list(customer="my_customer", query=f"orgUnitPath='{org_unit_path}'", maxResults=max_results, orderBy="email")
+            .list(
+                customer="my_customer",
+                query=f"orgUnitPath='{org_unit_path}'",
+                maxResults=max_results,
+                orderBy="email",
+            )
             .execute()
         )
         users = results.get("users", [])
@@ -111,16 +132,21 @@ class GoogleAdminDirectory:
             for user in users:
                 print(f"{user['primaryEmail']} ({user['name']['fullName']})")
 
-    def insert_user(self, primary_email, given_name, family_name, password, org_unit_path="/", changePasswordAtNextLogin=False):
+    def insert_user(
+        self,
+        primary_email,
+        given_name,
+        family_name,
+        password,
+        org_unit_path="/",
+        changePasswordAtNextLogin=False,
+    ):
         user_info = {
             "primaryEmail": primary_email,
-            "name": {
-                "givenName": given_name,
-                "familyName": family_name
-            },
+            "name": {"givenName": given_name, "familyName": family_name},
             "password": password,
             "orgUnitPath": org_unit_path,
-            "changePasswordAtNextLogin": changePasswordAtNextLogin
+            "changePasswordAtNextLogin": changePasswordAtNextLogin,
         }
         try:
             user = self.service.users().insert(body=user_info).execute()
@@ -130,60 +156,73 @@ class GoogleAdminDirectory:
             if e.reason == "Entity already exists.":
                 print(f"[CANCELADO] Usuário já registrado! {user_info['primaryEmail']}")
             return False
-    
+
     def delete_user(self, primary_email):
         try:
             self.service.users().delete(userKey=primary_email).execute()
             print(f"[REMOVIDO] Usuário {primary_email} removido.")
         except Exception as e:
             print(f"An error occurred: {e}")
-    
-    def update_user(self, primary_email, given_name=None, family_name=None, new_primary_email=None, password=None, org_unit_path=None, change_password_at_next_login=False):
+
+    def update_user(
+        self,
+        primary_email,
+        given_name=None,
+        family_name=None,
+        new_primary_email=None,
+        password=None,
+        org_unit_path=None,
+        change_password_at_next_login=False,
+    ):
         user_info = {}
-        
+
         if given_name or family_name:
             user_info["name"] = {}
             if given_name:
                 user_info["name"]["givenName"] = given_name
             if family_name:
                 user_info["name"]["familyName"] = family_name
-        
+
         if new_primary_email:
             user_info["primaryEmail"] = new_primary_email
-        
+
         if password:
             user_info["password"] = password
-        
+
         if org_unit_path:
             user_info["orgUnitPath"] = org_unit_path
-        
+
         # Adiciona a configuração para forçar mudança de senha no próximo login
         user_info["changePasswordAtNextLogin"] = change_password_at_next_login
-        
+
         if not user_info:
             print("No update parameters provided.")
             return
-        
+
         try:
-            updated_user = self.service.users().update(userKey=primary_email, body=user_info).execute()
+            updated_user = (
+                self.service.users()
+                .update(userKey=primary_email, body=user_info)
+                .execute()
+            )
             print(f"User {primary_email} updated successfully.")
         except Exception as e:
             print(f"An error occurred: {e}")
 
     def read_novos_alunos(self):
         if not os.path.exists(self.file_path):
-            print("File not found: novos_alunos.xlsx")
+            print(f"File not found: {self.file_path}")
             return []
-        
+
         df = pd.read_excel(self.file_path)
         return df.to_dict(orient="records")
-    
+
     def prepare_users_from_xlsx(self):
         alunos = self.read_novos_alunos()
         if not alunos:
             print("No student data found.")
             return []
-        
+
         prepared_users = []
         for aluno in alunos:
             mat = aluno.get("mat")
@@ -192,27 +231,29 @@ class GoogleAdminDirectory:
             family_name = " ".join(nome_completo[1:]) if len(nome_completo) > 1 else ""
             primary_email = f"{mat}@smec.saquarema.rj.gov.br"
             password = self.defaultPassword
-            org_unit_path = "/"
-            
-            prepared_users.append({
-                "primaryEmail": primary_email,
-                "givenName": given_name,
-                "familyName": family_name,
-                "password": password,
-                "orgUnitPath": org_unit_path
-            })
-        
+            org_unit_path = "/ESCOLAS/Escola Municipal Luciana Santana Coutinho"
+
+            prepared_users.append(
+                {
+                    "primaryEmail": primary_email,
+                    "givenName": given_name,
+                    "familyName": family_name,
+                    "password": password,
+                    "orgUnitPath": org_unit_path,
+                }
+            )
+
         return prepared_users
-    
+
     def insert_users_from_prepared_list(self, prepared_users):
-        for idx ,user in enumerate(prepared_users):
+        for idx, user in enumerate(prepared_users):
             status = "CANCELADO"
             res = self.insert_user(
-                user["primaryEmail"], 
-                user["givenName"], 
-                user["familyName"], 
-                user["password"], 
-                user["orgUnitPath"]
+                user["primaryEmail"],
+                user["givenName"],
+                user["familyName"],
+                user["password"],
+                user["orgUnitPath"],
             )
 
             if res:
@@ -223,15 +264,15 @@ class GoogleAdminDirectory:
     def convert_md_to_xlsx(self, markdown_file, output_xlsx):
         with open(markdown_file, "r", encoding="utf-8") as f:
             md_content = f.readlines()
-        
+
         data = []
         for line in md_content:
-            if "|" in line: #and not line.startswith("|"):
+            if "|" in line:  # and not line.startswith("|"):
                 columns = [col.strip() for col in line.split("|") if col.strip()]
                 print(columns)
                 if len(columns) == 3:
                     data.append({"mat": columns[2], "nome": columns[1]})
-        
+
         if data:
             df = pd.DataFrame(data)
             df.to_excel(output_xlsx, index=False)
@@ -244,47 +285,50 @@ class GoogleAdminDirectory:
         if not alunos:
             print("No student data found.")
             return
-        
+
         for aluno in alunos:
             mat = aluno.get("mat")
             primary_email = f"{mat}@smec.saquarema.rj.gov.br"
             self.delete_user(primary_email)
-    
+
     def update_status_in_xlsx(self, row_index, status):
         if not os.path.exists(self.file_path):
             print("Arquivo não encontrado:", self.file_path)
             return
-        
+
         df = pd.read_excel(self.file_path)
         if "status" not in df.columns:
             df["status"] = ""
-        
+
         df.at[row_index, "status"] = status
         df.to_excel(self.file_path, index=False)
         print(f"Status atualizado na linha {row_index}: {status}")
 
-    def reset_passwords_from_xlsx(self, new_password="provisorio2025", force_change_at_login=True):
+    def reset_passwords_from_xlsx(
+        self, new_password="provisorio2026", force_change_at_login=True
+    ):
         if not os.path.exists(self.file_path):
             print("Arquivo não encontrado:", self.file_path)
             return
-        
+
         df = pd.read_excel(self.file_path)
-        if df.empty or "email" not in df.columns:
-            print("Arquivo não possui uma coluna 'email'.")
+        if df.empty or "mat" not in df.columns:
+            print("Arquivo não possui uma coluna 'matricula'.")
             return
-        
+
         print(f"Iniciando redefinição de senhas para: {new_password}")
         if force_change_at_login:
             print("Usuários serão forçados a alterar a senha no próximo login.")
-        
+
         for idx, row in df.iterrows():
-            email = row["email"]
+            mat = row["mat"]
+            email = f"{mat}@smec.saquarema.rj.gov.br"
             if pd.notna(email):
                 try:
                     self.update_user(
                         primary_email=email,
                         password=new_password,
-                        change_password_at_next_login=force_change_at_login
+                        change_password_at_next_login=force_change_at_login,
                     )
                     status_msg = "SENHA ATUALIZADA"
                     if force_change_at_login:
@@ -297,29 +341,47 @@ class GoogleAdminDirectory:
             else:
                 print(f"Linha {idx}: email vazio ou inválido")
                 df.at[idx, "status"] = "EMAIL INVÁLIDO"
-        
+
         df.to_excel(self.file_path, index=False)
         print("Processo de redefinição de senhas concluído.")
-    
+
     def resumir_nome(self, nome: str) -> str:
         # palavras que devem ser removidas
         ignorar = {"de", "da", "do", "dos"}
-        
+
         # transforma em minúsculo e divide por espaços
         partes = nome.lower().split()
-        
+
         if len(partes) == 0:
             return ""
-        
+
         # primeiro nome completo
         primeiro = partes[0]
-        
+
         # último sobrenome completo
         ultimo = partes[-1]
-        
+
         # sobrenomes do meio (descartando os "de", "da", etc.)
         meio = [p[0] for p in partes[1:-1] if p not in ignorar]
-        
+
         # junta tudo
         resultado = primeiro + "".join(meio) + ultimo
         return resultado
+
+    def criar_sala(self, parent: str, nome_sala: str) -> None:
+        body = {"name": nome_sala, "parentOrgUnitPath": parent}
+
+        try:
+            orgunit = (
+                self.service.orgunits()
+                .insert(customerId="my_customer", body=body)
+                .execute()
+            )
+
+            print(f"✅ Sala criada: {orgunit['orgUnitPath']}")
+
+        except HttpError as e:
+            if e.resp.status == 409:
+                print(f"⚠️ Sala já existe: {nome_sala}")
+            else:
+                print(f"❌ Erro ao criar sala {nome_sala}: {e}")
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from gad import GoogleAdminDirectory
 from conversor_json import converter_json_para_lista
+from createRooms import criar_sala
 import json
 
-admin_directory = GoogleAdminDirectory("arquivo_formatado.xlsx")
+admin_directory = GoogleAdminDirectory("alunos_gustavo_formatado.xlsx")
 
 # LISTAR TODOS OS USUARIOS, COM LIMITE DE REGISTROS-------------------------
 # admin_directory.list_users(20)
@@ -18,32 +19,46 @@ admin_directory = GoogleAdminDirectory("arquivo_formatado.xlsx")
 alunos = admin_directory.read_novos_alunos()
 # print(alunos)
 
+
 # alunos = [
-#     {"nome": "Aluno de exemplo", "matricula": "00000000000000"},
+#     {"nome": "Aluno de exemplo", "matriculicula": "00000000000000"},
 # ]
 
-for aluno in alunos[9630:]:
-    print(f"Processando aluno: {aluno['Nome']} - Matrícula: {aluno['Matricula']} Senha: {aluno['Senha']} - Unidade: {aluno['Unidade']}")
+for idx, aluno in enumerate(alunos, start=1):
+    if not aluno["matricula"] == None:
+        mat = aluno["matricula"]
+#        turma = int(aluno["Turma"])
+#        turma_str = str(turma)
+#        unidade = aluno["Unidade"]
+#        senha = aluno["senha"]
+        senha = "#gustavo123"
+#        unidade_para_sala = f"/ESCOLAS/{unidade}"
+#        if unidade_para_sala and turma_str:
+#            admin_directory.criar_sala(unidade_para_sala, turma_str)
+        print(f" ------------------")
+        print(f"Processando ID: {idx} MATRICULA: {mat} ")
     # TRATAR O NOME COMPLETO ---------------------------------------
-    nome_list = aluno["Nome"].split(" ")
-    # pre_mail = (f"{nome_list[0]}{nome_list[1][0]}{nome_list[-1]}").lower()
+        nome_list = aluno["nome"].split(" ")
+   # pre_mail = (f"{nome_list[0]}{nome_list[1][0]}{nome_list[-1]}").lower()
     # nome_temp = admin_directory.resumir_nome(aluno['Nome'])
-    
+
     # INSERIR NOVO USUARIO---------------------------------------------------
-    if admin_directory.insert_user(f'{aluno["Matricula"]}@smec.saquarema.rj.gov.br', nome_list[0], " ".join(nome_list[1:]), aluno["Senha"], f'/ESCOLAS/{aluno["Unidade"]}'):
-        print(f"Usuário {aluno['Matricula']} inserido com sucesso.")
-    else:        print(f"Falha ao inserir usuário {aluno['Matricula']}.")
+    if admin_directory.insert_user(f'{mat}@smec.saquarema.rj.gov.br', nome_list[0], " ".join(nome_list[1:]), senha, f"/ESCOLAS/Colégio Municipal Gustavo Campos Da Silveira"):
+        print(f"Usuário {mat} inserido com sucesso.")
+    else:        
+        print(f"Falha ao inserir usuário {mat}.")
 
     # OBTER EMAIL PELO NOME COMPLETO-----------------------------------------
     # admin_directory.get_email_by_fullname(aluno['Nome'])
-    
-    # ATUALIZAR USUARIO------------------------------------------------------
-    admin_directory.update_user(f"{aluno['Matricula']}@smec.saquarema.rj.gov.br", password=aluno["Senha"], org_unit_path=f"/ESCOLAS/{aluno['Unidade']}", change_password_at_next_login=False)
-    
-    # REMOVER USUARIO-------------------------------------------------------
-    # admin_directory.delete_user(f"{nome_temp}@smec.saquarema.rj.gov.br")
 
-# FIM DO LOOPING-------------------------------------------------------------
+    # ATUALIZAR USUARIO------------------------------------------------------
+    admin_directory.update_user(f'{mat}@smec.saquarema.rj.gov.br', password=senha, org_unit_path=f"/ESCOLAS/Colégio Municipal Gustavo Campos Da Silveira", change_password_at_next_login=False)
+
+    # REMOVER USUARIO-------------------------------------------------------
+#    admin_directory.delete_user(f"{aluno['Matricula']}@smec.saquarema.rj.gov.br")
+    print("===================")
+
+#FIM FIM DO LOOPING-------------------------------------------------------------
 #
 #
 
@@ -67,9 +82,9 @@ for aluno in alunos[9630:]:
     # print(f"Email do aluno: {data['primaryEmail']}")
 
 # ADICIONAR USUARIOS DO ARQUIVO EXCEL (novos_alunos.xlsx)
-# toadd = admin_directory.prepare_users_from_xlsx()
-# print(toadd)
-# admin_directory.insert_users_from_prepared_list(toadd)
+#toadd = admin_directory.prepare_users_from_xlsx()
+#print(toadd)
+#admin_directory.insert_users_from_prepared_list(toadd)
 
 # REMOVER USUARIOS DO ARQUIVO EXCEL (novos_alunos.xlsx)
 # admin_directory.delete_users_from_xlsx()
@@ -78,4 +93,4 @@ for aluno in alunos[9630:]:
 # admin_directory.convert_md_to_xlsx("test.md", "output.xlsx")
 
 # ATUALIZAR SENHA PELO ARQUIVO EXCEL
-# admin_directory.reset_passwords_from_xlsx()
+#admin_directory.reset_passwords_from_xlsx("luciana123", False)
